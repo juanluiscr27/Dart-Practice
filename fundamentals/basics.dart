ae599331dacb29e970d4c4296d26244fee706e10
Juan Luis Casanova <juanluiscr27@hotmail.com>
2023-05-20 18:42:07 -0400

    Added papckage import, the main function, static/dynamic type variables and print to the console

---
+++ fundamentals/basics.dart
@@ -0,0 +1,13 @@
+// To import the core package from dart
+import 'dart:core';
+
+/// The main function is the program entry point of execution
+void main() {
+  // Type inference (dynamically type)
+  var firstName = 'Juan Luis';
+  // Statically typed variable
+  String lastName = 'Casanova';
+
+  // Print to the console
+  print(firstName + ' ' + lastName);
+}
